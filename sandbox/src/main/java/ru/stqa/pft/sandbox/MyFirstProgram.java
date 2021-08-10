bae707165b71e4000389529637a6707e067784a7
Taurus <mikhaylov@exbico.com>
2021-08-10 13:55:33 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,38 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 
-	public static void main(String[] args) {
-		System.out.print("Hello, world");
-	}
+    public static void main(String[] args) {
+
+        hello("world");
+        hello("user");
+        int len3 = 5;
+        System.out.println("Площадь квадрата со стороной " + len3 + "=" + area(len3));
+
+
+        int a = 2;
+        int b = 3;
+        System.out.println("Площадь прямоугольника со сторонами:"+a+"и"+b+ " = " + area(a,b));
+
+
+
+        double d = 8.2;
+        double s = d * d;
+        System.out.println("Площадь квадрата со стороной " + d + "=" + s);
+
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static int area(int a) {
+        int s = a * a;
+        return s;
+    }
+
+    public static int area(int a, int b) {
+        int s = a * b;
+        return s;
+    }
+
 }
