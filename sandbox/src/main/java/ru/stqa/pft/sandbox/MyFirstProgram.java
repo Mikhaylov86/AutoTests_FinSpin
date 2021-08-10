1dba4b01ad7d82c5b91ab1b2c597d1972b6d3a05
Taurus <mikhaylov@exbico.com>
2021-08-10 14:20:32 +0500

    12

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,19 +7,20 @@ public class MyFirstProgram {
 
         hello("world");
         hello("user");
-        int len3 = 5;
-        System.out.println("Площадь квадрата со стороной " + len3 + "=" + area(len3));
 
+        Square s = new Square();
+        s.len = 5;
+        System.out.println("Площадь квадрата со стороной " + s.len + "=" + area(s));
 
-        int a = 2;
-        int b = 3;
-        System.out.println("Площадь прямоугольника со сторонами:"+a+"и"+b+ " = " + area(a,b));
-
+        Rectangle r = new Rectangle();
+        r.a = 2;
+        r.b = 3;
+        System.out.println("Площадь прямоугольника со сторонами:" + r.a + "и" + r.b + " = " + area(r));
 
 
         double d = 8.2;
-        double s = d * d;
-        System.out.println("Площадь квадрата со стороной " + d + "=" + s);
+        double p = d * d;
+        System.out.println("Площадь квадрата со стороной " + d + "=" + p);
 
     }
 
@@ -27,14 +28,12 @@ public class MyFirstProgram {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static int area(int a) {
-        int s = a * a;
-        return s;
+    public static double area(Square s) {
+        return s.len * s.len;
     }
 
-    public static int area(int a, int b) {
-        int s = a * b;
-        return s;
+    public static int area(Rectangle r) {
+        return r.a * r.b;
     }
 
 }
